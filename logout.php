ced849133213ccf7f103de480ac24139a3d34123
Wshhkey <alisegaf204@gmail.com>
2026-04-01 07:32:56 +0700

    Correct session variable unset in logout.php
    Fix session unset by removing 'siswa_kelas' and 'siswa_nama'.

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/config/app.php';
 session_start();
-unset($_SESSION['admin_user'], $_SESSION['siswa_nis'], $_SESSION['siswa_kelas'], $_SESSION['siswa_nama']);
+unset($_SESSION['admin_user'], $_SESSION['siswa_nis'];
 header('Location: ' . BASE_URL . '/index.php');
 exit;
